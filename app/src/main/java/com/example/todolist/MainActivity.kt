078848c96c335597f79f734467923b1b3f3b65ac
Filip Václavík <filip.vaclavik@datamix.eu>
2024-01-07 18:45:11 +0100

    Updated mainLayout and Added edit page

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editButton = findViewById<Button>(R.id.buttonNewItem);
+        editButton.setOnClickListener {
+            OpenEditPage();
+        }
+    }
+
+    fun OpenEditPage() {
+        val intent = Intent(this, editPage::class.java);
+        startActivity(intent);
     }
 }
