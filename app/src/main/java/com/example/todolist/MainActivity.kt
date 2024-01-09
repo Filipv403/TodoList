ace46f9cb474d8bc502e66d56f1f750f1f131daa
Filip Václavík <filip.vaclavik@datamix.eu>
2024-01-09 20:36:39 +0100

    Added splashScreen and Icon

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -4,10 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
 
         val editButton = findViewById<Button>(R.id.buttonNewItem);
