4384c9a0e51890e5b451260f3be766eab156d056
Filip Václavík <filip.vaclavik@datamix.eu>
2024-01-10 15:09:29 +0100

    Finished recyclerView, added clear btn and polishing

--- app/src/main/java/com/example/todolist/Item.kt
+++ app/src/main/java/com/example/todolist/Item.kt
@@ -4,8 +4,8 @@ import java.util.Date
 
 class Item(Subject: String, Description: String) {
     var Id: Int = 1;
-    var Subject: String = "";
-    var Description: String = "";
+    var Subject: String = Subject;
+    var Description: String = Description;
     //var RealizationDate: Date = Date();
     var Completed: Boolean = false;
     var Validity: Boolean = true;
--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,25 +1,50 @@
 package com.example.todolist
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: ItemViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContentView(R.layout.activity_main)
 
+        val sharedPreferences : SharedPreferences = getSharedPreferences("TODOLIST", Context.MODE_PRIVATE);
+
+        viewModel = ViewModelProvider(this, ItemViewModelFactory(sharedPreferences)).get(ItemViewModel::class.java);
+
+        val recyclerView = findViewById<RecyclerView>(R.id.itemsView);
+        val adapter = ItemAdapter(viewModel);
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
         val editButton = findViewById<Button>(R.id.buttonNewItem);
         editButton.setOnClickListener {
             OpenEditPage();
+        }
+
+        val clearButton = findViewById<Button>(R.id.buttonClear);
+        clearButton.setOnClickListener {
+            ClearStorage(sharedPreferences, adapter);
         }
     }
 
     fun OpenEditPage() {
         val intent = Intent(this, editPage::class.java);
         startActivity(intent);
+    }
+
+    fun ClearStorage(sharedPreferences : SharedPreferences, adapter: ItemAdapter) {
+        sharedPreferences.edit().clear().apply();
+        adapter.notifyDataSetChanged();
     }
 }
